9a19d535bcd2d9a4c3bd08876a6dfb9d384fdf07
zawataku <c1254976@st.kanazawa-it.ac.jp>
2024-06-14 11:03:58 +0900

    新環境への移行

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,121 @@
-<script setup>
-import html2canvas from 'html2canvas';
-import { ref } from 'vue';
-
-const name_kanji = ref('');
-const name_hiragana = ref('');
-
-const generate = () => {
-  const element = document.querySelector("#preview_inner");
-  html2canvas(element, {
-    scale: 2, // 高解像度にするためにスケールを調整
-    useCORS: true // クロスオリジンリソースをキャプチャするために必要な場合
-  }).then(function (canvas) {
-    var result = document.querySelector(".result");
-    if (result) {
-      result.innerHTML = '';
-      result.appendChild(canvas);
-    } else {
-      console.error('Result element not found');
-    }
-  });
-};
-</script>
-
 <template>
-  <div class="text-center text-2xl font-bold">
-    <h1>Welcome to</h1>
-    <h1>北陸鉄道石川線 駅名標ジェネレータ</h1>
-  </div>
-
-  <div id="preview" class="block">
-    <div id="preview_inner" class="w-96 h-48 mx-auto relative flex items-center justify-center">
-      <div id="name_hiragana" class="absolute inset-0 top-8 flex justify-center text-center text-4xl">{{ name_hiragana
-        }}</div>
-      <div id="name_kanji" class="absolute inset-0 top-20 flex justify-center text-center text-base">{{ name_kanji }}
+  <div id="app">
+    <div class="inner">
+      <div class="header mb-4">
+        <h1 class="header-inner">北鉄石川線 駅名標ジェネレータ</h1>
       </div>
-      <img src="./assets/board_base.jpg" class="w-full h-full object-cover" alt="">
+      <div class="main">
+        <canvas class="canvas" ref="canvas" width="1280" height="720"></canvas>
+        <img class="output-image" ref="outputImage" :src=imageDataUrl alt="Generated Image" />
+      </div>
+      <div class="forms">
+        <p>右クリックメニュー「名前を付けて画像を保存」またはロングタップで保存できます</p>
+        <h2>駅の設定</h2>
+        <input class="form-control my-2 mx-auto" v-model="text0" placeholder="Type Here" />
+        <input class="form-control my-2 mx-auto" v-model="text1" placeholder="Type Here" />
+        <input class="form-control my-2 mx-auto" v-model="text2" placeholder="Type Here" />
+      </div>
+      <div class="forms">
+        <h2>一つ前の駅の設定</h2>
+        <input class="form-control my-2 mx-auto" v-model="text0" placeholder="Type Here" />
+        <input class="form-control my-2 mx-auto" v-model="text1" placeholder="Type Here" />
+        <input class="form-control my-2 mx-auto" v-model="text2" placeholder="Type Here" />
+      </div>
+      <div class="footer mt-5">
+        <p>Version 1.0.0</p>
+        <p>Licensed under the MIT License</p>
+        <a href="https://github.com/zawataku/iskwline_board">GitHub Repository</a>
+      </div>
     </div>
   </div>
-  <div class="w-2/3 mx-auto my-5">
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="駅名（漢字）" v-model="name_kanji" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="駅名（ひらがな）" v-model="name_hiragana" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="駅名（ローマ字）" v-model="name_rome">
-    </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="次の駅名（ひらがな）" v-model="next_name_hiragana" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="次の駅名（ローマ字）" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="前の駅名（ひらがな）" />
-    </label>
-    <label class="input input-bordered flex items-center gap-2">
-      <input type="text" class="grow" placeholder="前の駅名（ローマ字）" />
-    </label>
-  </div>
-
-  <button class="btn-outline" @click="generate">Generate</button>
-
-  <div class="result"></div>
 </template>
+
+<script>
+import html2canvas from 'html2canvas';
+
+export default {
+  data() {
+    return {
+      text0: '',
+      text1: '',
+      text2: '',
+      image: null,
+      fontsLoaded: false,
+      imageDataUrl: '/images/loading.webp'
+    };
+  },
+  watch: {
+    text0() {
+      this.drawText();
+    },
+    text1() {
+      this.drawText();
+    },
+    text2() {
+      this.drawText();
+    },
+  },
+  mounted() {
+    this.loadResources();
+  },
+  methods: {
+    async loadResources() {
+      await this.loadFonts();
+      await this.loadImage();
+      this.drawText();
+    },
+    loadFonts() {
+      return new Promise((resolve) => {
+        const font0 = new FontFace('Font_0', 'url(/fonts/GENJYUUGOTHICX-BOLD.woff2)');
+        Promise.all([font0.load()]).then((loadedFonts) => {
+          loadedFonts.forEach((font) => document.fonts.add(font));
+          this.fontsLoaded = true;
+          resolve();
+        });
+      });
+    },
+    loadImage() {
+      return new Promise((resolve) => {
+        const img = new Image();
+        img.src = '/images/board_base.png'; // 画像のパスを指定
+        img.onload = () => {
+          this.image = img;
+          resolve();
+        };
+      });
+    },
+    drawText() {
+      if (!this.fontsLoaded || !this.image) return; // フォントと画像がロードされるのを待つ
+
+      const canvas = this.$refs.canvas;
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height); // キャンバスをクリア
+      if (this.image) {
+        ctx.drawImage(this.image, 0, 0, canvas.width, canvas.height); // 画像を描画
+      }
+      ctx.textAlign = 'center'; // テキストを中央揃えに設定
+      const x = canvas.width / 2; // キャンバスの幅の中央
+
+      // テキスト0を描画
+      ctx.font = '22px Font_0'; // ここでフォントを変更
+      ctx.fillStyle = 'white';
+      const y0 = 278;
+      ctx.fillText(this.text0, x, y0); // テキストを描画
+
+      // テキスト1を描画
+      ctx.font = '122px Font_0';
+      ctx.fillStyle = 'black';
+      const y1 = 453;
+      ctx.fillText(this.text1, x, y1); // テキストを描画
+
+      // テキスト2を描画
+      ctx.font = '40px Font_0'; // ここでフォントを変更
+      ctx.fillStyle = '#79C4B5';
+      const y2 = 550;
+      ctx.fillText(this.text2, x, y2); // テキストを描画
+
+      this.imageDataUrl = canvas.toDataURL('image/png');
+    },
+  },
+};
+</script>
